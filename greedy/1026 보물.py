342971b21f8ff1956cca003a288e8d3b48ab67c5
chahyeonnaa <gusdk21650@naver.com>
2023-01-09 09:32:24 +0900

    [BOJ] silver4 성공

--- greedy/1026 보물.py
+++ greedy/1026 보물.py
@@ -14,18 +14,18 @@
 # ---
 
 # +
-N=int(input())
+# 1026 보물
+# B는 재배열하면 안되는 것이 핵심
+# A 오름차순 정렬하고, 앞에서부터 하나씩 B의 최댓값과 더한다.
 
+N=int(input())
 A=list(map(int, input().split()))
 B=list(map(int, input().split()))
-
+A.sort()
 result=0
-
-for _ in range(N):
-    result += min(A)*max(B)
-    A.remove(min(A))
+for i in range(N):
+    result += A[i]*max(B)
     B.remove(max(B))
     
 print(result)
 
-       
