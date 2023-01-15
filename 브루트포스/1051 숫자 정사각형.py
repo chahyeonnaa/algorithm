d464325edfe765fbf6fde42a364830336b6bad04
chahyeonnaa <gusdk21650@naver.com>
2023-01-15 14:01:22 +0900

    [BOJ] silver4 성공

--- 브루트포스/1051 숫자 정사각형.py
+++ 브루트포스/1051 숫자 정사각형.py
@@ -16,33 +16,25 @@
 # +
 # N이 세로
 # 반복문을 중간에 나오려고 하면 힘들다. 걍 다 돌게 해야함
-# i,j 반복문 안에서 k를 도는것과 k안에서 i,j 돌리기 이해하기
 N,M=map(int, input().split())
 square=[]
 A=min(N,M)
 result=0
+
 for i in range(N):
     S=list(input())
     square.append(S)
 
-for k in range(A-1,-1,-1):
-    for i in range(M):
-        if result==1:
-            break
-        for j in range(N):
-            
-            if i+k>A or j+k>=A:
-                continue
+for i in range(N):
+    for j in range(M):
+        for k in range(A):
+            if i+k<N and j+k<M:
+                q1=square[i][j]
+                q2=square[i+k][j]
+                q3=square[i][j+k]
+                q4=square[i+k][j+k]
                 
-            q1=square[i][j]
-            q2=square[i+k][j]
-            q3=square[i][j+k]
-            q4=square[i+k][j+k]
-        
-            if q1==q2==q3==q4:
-                print((k+1))
-                result=1
-                break
-# -
-
-
+                if q1==q2==q3==q4:
+                    result=max(result,(k+1)**2)
+                
+print(result)
