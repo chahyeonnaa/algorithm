e031badbaa14be253f11c081f9ac5159a6f88706
chahyeonnaa <gusdk21650@naver.com>
2023-01-25 11:13:12 +0900

    [BOJ] silver4 해결

--- 정렬/1920 수 찾기.py
+++ 정렬/1920 수 찾기.py
@@ -46,3 +46,51 @@ for i in range(Mp):
         print(0)
     else:
         print(1)
+# +
+# 이분 탐색으로 풀기
+N=int(input())
+A=list(map(int, input().split()))
+A.sort()
+m=int(input())
+M=list(map(int, input().split()))
+
+def binary(L, A, start, end):
+    if start > end:
+        return 0
+    m =(start+end)//2
+    if L ==A[m]:
+        return 1
+    elif L <A[m]:
+        return binary(L, A, start, m-1)
+    else:
+        return binary(L, A, m+1, end)
+    
+for L in M:
+    start=0
+    end=len(A)-1
+    print(binary(L, A, start, end))
+
+
+
+
+
+
+
+
+# +
+# 집합으로 풀기
+# 
+N=int(input())
+A=set(input().split())
+m=int(input())
+M=list(input().split())
+
+
+for i in M:
+    if i in A:
+        print("1")
+    else:
+        print("0")
+# -
+
+
