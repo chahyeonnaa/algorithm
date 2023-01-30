6dcfdefe12b92ea23c65b9033a646e20144cb30d
chahyeonnaa <gusdk21650@naver.com>
2023-01-30 11:00:47 +0900

    [BOJ] silver5 성공

---
+++ 정렬/10867 중복 빼고 정렬하기.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+N=int(input())
+S=list(set(map(int, input().split())))
+S.sort()
+print(*S)
+
+
