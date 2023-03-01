712ff2767f5b716da32b4360621e0318bd1c166d
chahyeonnaa <gusdk21650@naver.com>
2023-03-01 12:30:44 +0900

    [BOJ] silver3 성공

---
+++ 백트래킹/10974 모든 순열.py
@@ -0,0 +1,33 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+N=int(input())
+S=[]
+
+def dfs():
+    if len(S)==N:
+        print(*S)
+        return
+    for i in range(1, N+1):
+        if i not in S:
+            S.append(i)
+            dfs()
+            S.pop()
+            
+dfs()
+# -
+
+
