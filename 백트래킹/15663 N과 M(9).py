af8f664010b7d4ba15465ea48e897b7d38b2dc6f
chahyeonnaa <gusdk21650@naver.com>
2023-03-06 14:28:40 +0900

    [BOJ] silver3 실패

---
+++ 백트래킹/15663 N과 M(9).py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+def dfs():
+    if len(result)==M:
+        print(*result)
+        return
+    A=0
+    for i in range(N):
+        if visited[i]==0 and A !=S[i]:
+            visited[i]=1
+            result.append(S[i])
+            A=S[i]
+            dfs()
+            visited[i]=0
+            result.pop()
+            
+N,M=map(int, input().split())
+S=list(map(int, input().split()))
+S.sort()
+visited=[0]*N
+result=[]
+dfs()
+# -
+
+
