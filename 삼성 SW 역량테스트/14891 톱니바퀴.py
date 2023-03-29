80822ca21cdd2f1f11443fa5e344a45875034583
chahyeonnaa <gusdk21650@naver.com>
2023-03-29 16:15:27 +0900

    [BOJ] gold5 코드

--- 삼성 SW 역량테스트/14891 톱니바퀴.py
+++ 삼성 SW 역량테스트/14891 톱니바퀴.py
@@ -48,3 +48,50 @@
 # ㅠㅠ 갈길이 멀다. 
 
 
+
+# +
+# visited 체커도 꼭 써야함
+# 아니면 돌렸던 톱니를 또 돌린다...
+def clock(graph):
+    temp=graph[7]
+    for i in range(6,-1,-1):
+        graph[i+1]=graph[i]
+    graph[0]=temp
+def clock_ban(graph):
+    temp=graph[0]
+    for i in range(7):
+        graph[i]=graph[i+1]
+    graph[7]=temp
+
+def dfs(x,y):
+    global visited
+    if visited[x]==0:
+        visited[x]=1
+        left=graph[x][6]
+        right=graph[x][2]
+        if y==1:
+            clock(graph[x])
+        else:
+            clock_ban(graph[x])
+            
+        if x-1>=0 and left!=graph[x-1][2]:
+            dfs(x-1,-y)
+        if x+1<=3 and right!=graph[x+1][6]:
+            dfs(x+1,-y)
+graph=[]
+for i in range(4):
+    graph.append(list(input().rstrip()))
+K=int(input())
+for i in range(K):
+    A,B=map(int, input().split())
+    visited=[0]*4
+    dfs(A-1,B)
+
+cnt=0
+for i in range(4):
+    if graph[i][0]=="1":
+        cnt += (2**i)
+print(cnt)
+# -
+
+
